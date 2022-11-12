5ae842e929a95aa4879953352565384ff40ad39f
Currae <114950361+Currae@users.noreply.github.com>
2022-11-12 23:35:13 +0300

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -7,7 +7,7 @@ package check.leap.year;
 
 /**
  *
- * @author Magdahlyne
+ * @author Magdaline
  */
 import java.util.Scanner;
 public class LeapYear {
@@ -15,11 +15,12 @@ public class LeapYear {
     public static void main(String[] args) {
        int year;
     	Scanner sc = new Scanner(System.in);
+        //Enter the year
     	System.out.println("Enter any Year:");
     	year = sc.nextInt();
     	sc.close();
         boolean isLeap = false;
-
+        //getting if it is a leap year or not
         if(year % 4 == 0)
         {
             if( year % 100 == 0)
