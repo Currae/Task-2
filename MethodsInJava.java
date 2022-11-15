114e3a889404b768d055e508eef384b32a1d3926
Currae <114950361+Currae@users.noreply.github.com>
2022-11-15 23:14:55 +0300

    Update MethodsInJava.java

--- MethodsInJava.java
+++ MethodsInJava.java
@@ -29,10 +29,10 @@ public class MethodsInJava {
             System.out.println(n1 + " is the largest number.");
 
         else if (n2 >= n1 && n2 >= n3)
-            System.out.println(n2 + " is the largest number.");
+            System.out.println(n3 + " is the smallest number.");
 
         else
-            System.out.println(n3 + " is the largest number.");
+            System.out.println(n2 + " is the largest number.");
       
     }
 }
